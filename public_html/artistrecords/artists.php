ae9a0206db6d911e088e4f1c03bf6c3755b4a543
robomartion <robomartion@hotmail.com>
2015-04-28 09:44:19 +1000

    database php code
    A bug with updating entries needs to be sorted out as well when making
    an entry with a large amount of text. The displayartists page needs to
    only show a preview of the artist details. The details page still needs
    to be coded so that it only shows one artist based on which link you
    clicked.

---
+++ public_html/artistrecords/artists.php
@@ -0,0 +1,87 @@
+<?php
+include("dbconnect.php")
+/* Fairly simple example - there's a form for inserting a new artist record and a set of forms, one for each record,
+	that allows for deleting and updating each record. In these ones, the id of the record is passed using a hidden form field. 
+*/
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP SQLite Database (Artist Records)</title>
+<style type="text/css">
+.subtleSet {
+	border-radius:25px;
+	width: 30em;
+}
+.deleteButton {
+	color: red;
+}
+</style>
+</head>
+
+<body>
+<h1>Artist Database</h1>
+
+<form id="insert" name="insert" method="post" action="dbprocessartist.php" enctype="multipart/form-data">
+<fieldset class="subtleSet">
+    <h2>Insert new artist record:</h2>
+    <p>
+      <label for="artist">Artist: </label>
+      <input type="text" name="artist" id="artist">
+    </p>
+    <p>
+      <label for="details">Details: </label>
+      <input type="text" name="details" id="details">
+    </p>
+    <p>
+        Select image to upload:
+        <input action="upload.php" method="post" enctype="multipart/form-data" type="file" name="fileToUpload" id="fileToUpload">
+    </p>
+    <p>
+      <input type="submit" name="submit" id="submit" value="Insert Entry">
+    </p>
+</fieldset>
+</form>
+
+<fieldset class="subtleSet">
+<h2>Current data:</h2>
+    <table>
+        <tr>
+            <td>
+                <h3>Artist</h3>
+            </td>
+            <td>
+                <h3>Details</h3>
+            </td>
+            <td>
+                <h3>Image</h3>
+            </td>
+        </tr>
+<?php
+// Display what's in the database at the moment.
+$sql = "SELECT * FROM artist";
+foreach ($dbh->query($sql) as $row)
+{
+?>
+<form id="deleteForm" name="deleteForm" method="post" action="dbprocessartist.php" enctype="multipart/form-data">
+<?php
+	echo "<tr><td><input type='text' name='artist' value='$row[artist]'></td>
+   <td><input type='text' name='details' id='details' value='$row[details]'></td>
+   <td><input action='upload.php' method='post' enctype='multipart/form-data' type='file' name='fileToUpload' id='fileToUpload'></td>
+    <td><img src= 'uploads/$row[image]' width=50px></td></table>\n";
+
+    echo "<input type='hidden' name='id' value='$row[id]'/>\n";
+
+?>
+<input type="submit" name="submit" value="Update Entry" />
+<input type="submit" name="submit" value="X" class="deleteButton">
+</form>
+<?php
+}
+echo "</fieldset>\n";
+// close the database connection
+$dbh = null;
+?>
+</body>
+</html>
---
+++ public_html/artistrecords/details.php
@@ -0,0 +1,40 @@
+<?php include( "dbconnect.php") /* Fairly simple example - there 's a form for inserting a new artist record and a set of forms, one for each record,
+	that allows for deleting and updating each record. In these ones, the id of the record is passed using a hidden form field. 
+*/
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP SQLite Database (Artist Records)</title>
+<style type="text/css">
+.subtleSet {
+	border-radius:25px;
+	width: 30em;
+}
+.deleteButton {
+	color: red;
+}
+</style>
+</head>
+
+<body>
+<h1><?php $sql = "SELECT * FROM artist";
+    foreach ($dbh->query($sql) as $row) {
+        echo "$row[artist]";
+    }
+    ?>
+</h1>
+<table>
+    <?php
+    echo "<tr><td>$row[details]</td><td><img src= 'uploads/$row[image]' width=300px></td></tr>";
+    $dbh  = null;
+    ?>
+</table>
+<?php
+echo "</fieldset>\n";
+// close the database connection
+    $dbh=null;
+?>
+    </body>
+    </html>
