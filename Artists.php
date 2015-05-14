07ce1126df87e207637e1092241af31176d2bab2
Josh Page <joshua.page1@my.jcu.edu.au>
2015-05-15 08:44:35 +1000

    PHP pages need coding with PHP
    just made these using the current html pages

---
+++ Artists.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9">
+<title>TCMC - Townsville Community Music Centre</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="wrapper">
+  <div id="content">
+    <header id="top"> <a href="index.html"><img src="pictures/logowhite.png" width="200" height="123" alt=""/></a>
+      <nav id="mainnav">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="Events.html">Events</a></li>
+          <li><a href="Artists.html" class="thispage">Artists</a></li>
+          <li><a href="About.html">About</a></li>
+        </ul>
+      </nav>
+      
+      <div id="second_nav">
+          <div id="BUY_TICKETS">Buy Tickets</div>
+          <div id="PLAY_FOR_US">Play For Us</div>
+          <div id="BECOME_A_MEMBER">Become a Member</div>
+          <div id="BECOME_A_VOLUNTEER">Become a Volunteer</div>
+      </div>
+    </header>
+  </div>
+  
+ <div id="artists_background0"><img src="pictures/BlackBox.jpg" width="1692" height="350" alt=""/></div>
+ <div id="artists_background1"><img src="pictures/large_white_box_grayborder.jpg" width="800" height="255" alt=""/></div>
+ 
+<body1 id="body1_artists">
+
+<div id="artists_artist1"> <img src="pictures/Harbourside.jpg" width="200" height="200" alt=""/> </div>
+<div id="artist_name">
+  <h3>Harbourside Duo</h3>
+  
+  <div id="artists_events_text">
+      <h4>Upcoming Events:</h4>
+      <div id="event1_details"> <u>Dream Serenade</u> </div>
+      <div id="more_info1"> <u> More Info </u> </div>
+  </div>
+ <div id="artists_background3"><img src="pictures/large_white_box_grayborder.jpg" width="800" height="255" alt=""/></div>  
+</div>
+</body1> 
+ 
+<footer id="footer">
+<div id="contact"><strong><h2>Contact</h2></strong> <br> Phone: 07 4724 2086 <br> Mobile: 0402 255 182 <br> Email Address: admin@townsvillemusic.org.au <br> Postal Address: PO Box 1006, Townsville, Qld 4810 <br> Street Address: Townsville Civic Theatre, 41 Boundary Street, Townsville, Qld 4810 
+<div id="fblogo"> <a href="https://www.facebook.com/pages/Townsville-Community-Music-Centre/159636880763534?fref=ts"><img src="pictures/fblogo.jpg" width="64" height="64" alt=""/></a></div>
+
+<div id="links1"><a href="index.html">Home</a><br> Artists<br>Events<br>About </div> <div id="links2"> Buy Tickets<br>Become a Volunteer<br>Become a member<br>Play for Us </div>
+</div>
+<div id="sponsors"> <strong><h2> Sponsors</h2> </strong> 
+<div id="sponsor1"> <a href="http://www.townsville.qld.gov.au/Pages/default.aspx"><img src="pictures/1152800_1_M.png" width="60" height="86" alt=""/></a> </div>
+<div id="sponsor2"> <a href="https://www.qld.gov.au/index.html"><img src="pictures/qg-coa-ogp.png" width="86" height="86" alt=""/></a> </div>
+<div id="sponsor3"> <img src="pictures/1525593_1_O.png" width="100" height="80" alt=""/> </div>
+ </div>
+
+</footer>
+</div>     
+   
+</body>
+</html>
---
+++ Events.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>TCMC - Events</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="wrapper">
+  <div id="content">
+  <header id="top">
+	<img src="TCMC98Neg.jpg" width="175" height="113" alt=""/>
+    <nav id="mainnav">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="Events.html" class="thispage">Events</a></li>
+        <li><a href="Artists.html">Artists</a></li>
+        <li><a href="About.html">About</a></li>
+      </ul>  
+    </nav>    
+    <div id="VOLUNTEER">VOLUNTEER</div>
+    <div id="BECOME_A_MEMBER">BECOME A MEMBER</div>
+    <div id="ARTIST_SIGN-UP">ARTIST SIGN-UP</div>
+    <div id="BUY_TICKETS">BUY TICKETS</div>
+  </header>  
+
+</div>
+<div id="footer">
+	<div id="footer_content"><h2>Contact</h2> Phone: 07 4724 2086 <br> Mobile: 0402 255 182 <br> Email Address: admin@townsvillemusic.org.au <br> Postal Address: PO Box 1006, Townsville, Qld 4810 <br> Street Address: Townsville Civic Theatre, 41 Boundary Street, Townsville, Qld 4810</div>
+    <a href="https://www.facebook.com/pages/Townsville-Community-Music-Centre/159636880763534?fref=ts"><img src="fblogo.jpg" width="64" height="64" alt=""/></a></div>
+</div>
+</body>
+</html
